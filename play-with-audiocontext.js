e8326ba9c71064f0a7a4e2e54468f4d91f5c2fd5
routmoute <lezotre@gmail.com>
2022-03-28 04:36:51 +0200

    first commit

---
+++ play-with-audiocontext.js
@@ -0,0 +1,433 @@
+const AudioContex = window.AudioContext || window.webkitAudioContext;
+if (!AudioContex) {
+    const errorText = "Your browser isn't compatible with AudioContext..";
+    window.alert(errorText);
+    throw new Error(errorText);
+}
+
+let audioCtx = new AudioContex();
+let allLines = [];
+
+let noMove = false;
+const canvasElem = document.getElementById('canvas');
+canvasElem.height = window.innerHeight;
+canvasElem.width = window.innerWidth;
+window.resize = function() {
+    canvasElem.height = window.innerHeight;
+    canvasElem.width = window.innerWidth;
+}
+const canvasContext = canvasElem.getContext('2d');
+
+let nodeList = [];
+
+function createNode(name, nodeSettings, inputs, outputs, audioNode) {
+    // Body
+    const nodeId = nodeList.push({htmlNode: document.createElement('div')}) - 1;
+    nodeList[nodeId].htmlNode.id = nodeId;
+    nodeList[nodeId].htmlNode.classList.add('node');
+    nodeList[nodeId].htmlNode.innerHTML = "<b>"+name+"</b><hr>"
+    nodeList[nodeId].audioNode = audioNode;
+
+    // Drag
+    nodeList[nodeId].htmlNode.onmousedown = function(e) {
+        if (noMove)
+            return;
+        e.preventDefault();
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = function() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        };
+        document.onmousemove = function(e) {
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            nodeList[nodeId].htmlNode.style.top = (nodeList[nodeId].htmlNode.offsetTop - pos2) + "px";
+            nodeList[nodeId].htmlNode.style.left = (nodeList[nodeId].htmlNode.offsetLeft - pos1) + "px";
+            nodeList[nodeId].htmlNode.style.right = "auto";
+            allLines.forEach(elem => {
+                if (!elem) return;
+                if (elem.input == nodeId) {
+                    elem.coords.x2 -= pos1;
+                    elem.coords.y2 -= pos2;
+                } else if (elem.output == nodeId) {
+                    elem.coords.x1 -= pos1;
+                    elem.coords.y1 -= pos2;
+                }
+            });
+            drawLines();
+        };
+    };
+
+    // Close Button
+    let close = document.createElement('div');
+    close.classList.add('close');
+    close.innerText = "X";
+    close.title = "Delete this node";
+    close.onclick = function(e) {
+        deleteNode(nodeId);
+    };
+    nodeList[nodeId].htmlNode.appendChild(close);
+
+    // Inputs
+    if (inputs == 1 || inputs == 2) {
+        let input = document.createElement('div');
+        input.classList.add('input');
+
+        let inputDot = document.createElement('div');
+        inputDot.classList.add('i-dot');
+        inputDot.title = "link input"
+        input.appendChild(inputDot);
+
+        let inputImg = document.createElement('span');
+        inputImg.innerText = "→";
+        input.appendChild(inputImg);
+
+        let inputSelect = document.createElement('span');
+        inputSelect.classList.add('select');
+        inputSelect.innerText = "x";
+        inputDot.onmouseenter = function() {
+            if (noMove)
+                inputSelect.style.display = "block";
+        }
+        inputDot.onmouseleave = function() {
+            inputSelect.style.display = "none";
+        }
+        inputDot.appendChild(inputSelect);
+
+        nodeList[nodeId].htmlNode.appendChild(input);
+    }
+
+    // Outputs
+    if (outputs == 1 || outputs == 2) {
+        let output = document.createElement('div');
+        output.classList.add('output');
+
+        let outputImg = document.createElement('span');
+        outputImg.innerText = "→";
+        output.appendChild(outputImg);
+
+        let outputDot = document.createElement('span');
+        outputDot.classList.add('o-dot');
+        outputDot.title = "link output";
+        output.appendChild(outputDot);
+
+        let outputSelect = document.createElement('span');
+        outputSelect.classList.add('select');
+        outputSelect.innerText = "x";
+        outputDot.onmouseenter = function() {
+            if (!noMove)
+                outputSelect.style.display = "block";
+        }
+        outputDot.onmouseleave = function() {
+            outputSelect.style.display = "none";
+        }
+        outputSelect.onmousedown = function(e) {
+            e.preventDefault();
+            noMove = true;
+            const x1 = e.clientX - canvasElem.offsetLeft;
+            const y1 = e.clientY - canvasElem.offsetTop;
+            const lineId = allLines.push({
+                coords: {x1: x1, y1: y1},
+                output: nodeId
+            }) - 1;
+            document.onmousemove = function(e) {
+                e.preventDefault();
+                const x2 = e.clientX - canvasElem.offsetLeft;
+                const y2 = e.clientY - canvasElem.offsetTop;
+                allLines[lineId].coords.x2 = x2;
+                allLines[lineId].coords.y2 = y2;
+                drawLines();
+            };
+            document.onmouseup = function(e) {
+                e.preventDefault();
+                document.onmouseup = null;
+                document.onmousemove = null;
+                const pointedElem = document.elementFromPoint(e.clientX, e.clientY);
+                if (pointedElem.classList.contains("select") && pointedElem.parentElement.classList.contains('i-dot') && pointedElem.parentElement.parentElement.parentElement.id != nodeId) {
+                    allLines[lineId].input = pointedElem.parentElement.parentElement.parentElement.id;
+                    if (nodeList[nodeId].audioNode && nodeList[allLines[lineId].input].audioNode)
+                        nodeList[nodeId].audioNode.connect(nodeList[allLines[lineId].input].audioNode);
+                } else {
+                    allLines[lineId] = null;
+                }
+                drawLines();
+                noMove = false;
+            };
+        };
+        outputDot.appendChild(outputSelect);
+
+        nodeList[nodeId].htmlNode.appendChild(output);
+    }
+
+    // Settings
+    nodeSettings.forEach(element => {
+        switch (element.type) {
+            case "label":
+                let label = document.createElement('div');
+                label.innerText = element.name;
+
+                element.func(label);
+
+                nodeList[nodeId].htmlNode.appendChild(label);
+                break;
+            case "button":
+                let button = document.createElement('button');
+                button.innerText = element.name;
+
+                element.func(button);
+
+                nodeList[nodeId].htmlNode.appendChild(button);
+                break;
+            case "number":
+                let numberDiv = document.createElement('div');
+                let number = document.createElement('input');
+                number.type = "number";
+                number.min = element.min;
+                number.max = element.max;
+                number.value = element.actual;
+                let numberMe = false;
+                number.onmouseenter = function() {
+                    if (!noMove) {
+                        numberMe = true;
+                        noMove = true;
+                    }
+                };
+                number.onmouseleave = function() {
+                    if (numberMe) {
+                        noMove = false;
+                        numberMe = false;
+                    }
+                }
+
+                element.func(number);
+
+                numberDiv.appendChild(number);
+                let numberSubSpan = document.createElement('span');
+                numberSubSpan.innerText = element.sub;
+                numberDiv.appendChild(numberSubSpan);
+                nodeList[nodeId].htmlNode.appendChild(numberDiv);
+                break;
+            case "slider":
+                let sliderDiv = document.createElement('div');
+
+                let slider = document.createElement('input');
+                slider.type = "range";
+
+                slider.min = element.min;
+                slider.max = element.max;
+                slider.value = element.actual;
+                let sliderMe = false;
+                slider.onmouseenter = function() {
+                    if (!noMove) {
+                        sliderMe = true;
+                        noMove = true;
+                    }
+                };
+                slider.onmouseleave = function() {
+                    if (sliderMe) {
+                        noMove = false;
+                        sliderMe = false;
+                    }
+                }
+
+                element.func(slider);
+
+                sliderDiv.appendChild(slider);
+                nodeList[nodeId].htmlNode.appendChild(sliderDiv);
+                break;
+        }
+    });
+
+    document.body.appendChild(nodeList[nodeId].htmlNode);
+    return nodeId;
+}
+
+function deleteNode(nodeId) {
+    nodeList[nodeId].htmlNode.remove();
+    if (nodeList[nodeId].audioNode) {
+        nodeList[nodeId].audioNode.disconnect();
+    }
+    allLines.forEach((elem, index) => {
+        if (elem) {
+            if (elem.input == nodeId) {
+                allLines[index] = null;
+                if (elem.output) {
+                    if (nodeList[elem.output].audioNode)
+                        nodeList[elem.output].audioNode.disconnect(nodeList[nodeId].audioNode);
+                }
+            } else if (elem.output == nodeId) {
+                allLines[index] = null;
+            }
+        }
+    });
+    nodeList[nodeId] = null;
+    drawLines();
+}
+
+function reset() {
+    nodeList.forEach(function(node, index) {
+        if (node && index != 0) {
+            deleteNode(index);
+        }
+    });
+    nodeList = [];
+    allLines = [];
+}
+
+function drawLines() {
+    canvasContext.clearRect(0, 0, canvasElem.width, canvasElem.height);
+    canvasContext.beginPath();
+    if (dark) {
+        canvasContext.fillStyle = "white";
+        canvasContext.strokeStyle = "white";
+    }
+    allLines.forEach(e => {
+        if (e && e.coords.y2) {
+            canvasContext.moveTo(e.coords.x1, e.coords.y1);
+            canvasContext.lineTo(e.coords.x2, e.coords.y2);
+        }
+    });
+    canvasContext.stroke();
+}
+
+// Create Destination
+const destinationNodeId = createNode("AudioContext Destination", [], 2);
+nodeList[destinationNodeId].htmlNode.style.top = "20px";
+nodeList[destinationNodeId].htmlNode.style.right = "20px";
+nodeList[destinationNodeId].htmlNode.style.left = "auto";
+nodeList[destinationNodeId].htmlNode.childNodes.forEach(child => {
+    if (child.classList.contains('close')) {
+        child.remove();
+        return;
+    }
+});
+nodeList[destinationNodeId].audioNode = audioCtx.destination;
+
+
+/////////////////////////////////////////////////////
+///////////////// Buttons functions /////////////////
+/////////////////////////////////////////////////////
+
+let dark = false;
+function darkMode() {
+    if (dark) {
+        document.body.style.setProperty("--text-color", "black");
+        document.body.style.setProperty("--border-color", "black");
+        document.body.style.setProperty("--bg-color", "white");
+        dark = false;
+        document.getElementById('darkButton').innerText = "Black";
+    } else {
+        document.body.style.setProperty("--text-color", "white");
+        document.body.style.setProperty("--border-color", "white");
+        document.body.style.setProperty("--bg-color", "black");
+        dark = true;
+        document.getElementById('darkButton').innerText = "White";
+    }
+    drawLines();
+}
+
+function createGainNode() {
+    let gain = audioCtx.createGain();
+    let sliderInput = null;
+    let numberInput = null;
+    createNode("Gain", [
+        {
+            type: "label",
+            name: "gain",
+            func: function(label) {}
+        },
+        {
+            type: "number",
+            min: 0,
+            max: 150,
+            actual: 100,
+            sub: "%",
+            func: function(number) {
+                numberInput = number;
+                numberInput.onchange = function() {
+                    gain.gain.value = this.value/100;
+                    sliderInput.value = this.value;
+                };
+            }
+        },
+        {
+            type: "slider",
+            min: 0,
+            max: 150,
+            actual: 100,
+            func: function(slider) {
+                sliderInput = slider;
+                sliderInput.oninput = function() {
+                    gain.gain.value = this.value/100;
+                    numberInput.value = this.value;
+                };
+            }
+        }
+    ], 2, 2, gain);
+}
+
+function createOscillatorNode() {
+    let oscillator = audioCtx.createOscillator();
+    let freqLabel = null;
+    let sliderInput = null;
+    let numberInput = null;
+    createNode("Oscillator", [
+        {
+            type: "label",
+            name: "frequency",
+            func: function(label) {
+                freqLabel = label;
+            }
+        },
+        {
+            type: "number",
+            min: 1,
+            max: 20000,
+            actual: 440,
+            sub: "Hz",
+            func: function(number) {
+                numberInput = number;
+                numberInput.onchange = function() {
+                    oscillator.frequency.value = this.value;
+                    sliderInput.value = this.value;
+                };
+            }
+        },
+        {
+            type: "slider",
+            min: 1,
+            max: 20000,
+            actual: 440,
+            func: function(slider) {
+                sliderInput = slider;
+                sliderInput.oninput = function() {
+                    numberInput.value = this.value;
+                    oscillator.frequency.value = this.value;
+                };
+            }
+        },
+        {
+            type: "button",
+            name: "start",
+            func: function(button) {
+                let started = false;
+                button.onclick = function() {
+                    if (started) {
+                        oscillator.stop();
+                        button.innerText = "start";
+                        started = false;
+                    } else {
+                        oscillator.start();
+                        button.innerText = "stop";
+                        started = true;
+                    }
+                }
+            }
+        }
+    ], 0, 1, oscillator);
+}
